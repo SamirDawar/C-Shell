f3e8f8f3c281405fe5872deb2ac2115d48d528b8
SamirDawar <samir.dawar01@gmail.com>
2025-12-18 14:53:37 -0500

    Finished process function

--- main.c
+++ main.c
@@ -99,3 +99,25 @@ char **lsh_split_line(char *line) {
 
 
 
+int lsh_launch(char **args) {
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+
+  if (pid == 0) {
+    //child process 
+    if(execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else {
+    //parent  process 
+    do {
+      wpid = waitpid(pid, &stats, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
+
